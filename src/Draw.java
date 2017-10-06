9852607fc38303f176cd13f7954257a6b1d30437
joonsson <joonsson@gmail.com>
2017-10-06 10:29:40 +0200

    Started over

--- src/Draw.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-
-public class Draw extends JPanel {
-    public void draw(GameState gameState) {
-        Graphics g;
-        Graphics2D g2d = (Graphics2D) g;
-    }
-}
---
+++ src/Normandy.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.EventQueue;
+
+public class Normandy extends JFrame {
+    public Normandy() {
+        initUI();
+    }
+    private void initUI() {
+        add(new Board());
+        setTitle("Normandy");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(BOARD_WIDTH, BOARD_HEIGHT);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(()-> {
+            Normandy ex = new Normandy();
+            ex.setVisible(true);
+        });
+    }
+}
